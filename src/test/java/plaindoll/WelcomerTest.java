2df049541619daaafc5f52e2bf350a20d3b433c3
root <root@debian-s-2vcpu-2gb-fra1-01>
2023-11-12 10:20:06 +0000

    Welcomer

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void testGetHunterMessage() {
+    assertThat(welcomer.getHunterMessage().contains("hunter"));
+	}
 }
